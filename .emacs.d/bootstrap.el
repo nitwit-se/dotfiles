4f28863097ea5ca1d0363ec1160a60546e7ee0f9
mark <mark@dixon.se>
2021-06-10 22:36:08 +0200

    Added /Users/mark/.emacs.d/bootstrap.el

---
+++ .emacs.d/bootstrap.el
@@ -0,0 +1,88 @@
+
+;; Bootstrapping code taken and modified from:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Sanemacs version 0.3.0 ;;;
+;;; https://sanemacs.com   ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; For performance
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
+
+(add-hook 'after-init-hook #'(lambda ()
+                               ;; restore after startup
+                               (setq gc-cons-threshold 800000)))
+
+;; https://blog.sumtypeofway.com/posts/emacs-config.html
+;; There appears to be some sort of kerfuffle regarding ELPA’s GPG keys. The details are beyond me,
+;; but installing this package silences associated warnings.
+;;(use-package gnu-elpa-keyring-update)
+
+
+;;; Disable menu-bar, tool-bar, and scroll-bar.
+(if (fboundp 'menu-bar-mode)
+    (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;;; Fix this bug:
+;;; https://www.reddit.com/r/emacs/comments/cueoug/the_failed_to_download_gnu_archive_is_a_pretty/
+(when (version< emacs-version "26.3")
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+
+;;; Setup package.el
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'load-path "~/.emacs.d/packages/org-super-links")
+(unless package--initialized (package-initialize))
+
+
+
+;; Setup use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+
+
+
+(use-package exec-path-from-shell
+   :ensure t)
+
+(when (memq window-system '(mac ns x))
+   (exec-path-from-shell-initialize))
+
+;;; Put Emacs auto-save and backup files to /tmp/ or C:/Temp/
+(defconst emacs-tmp-dir (expand-file-name (format "emacs%d" (user-uid)) temporary-file-directory))
+(setq
+   backup-by-copying t                                        ; Avoid symlinks
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t
+   auto-save-list-file-prefix emacs-tmp-dir
+   auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t))  ; Change autosave dir to tmp
+   backup-directory-alist `((".*" . ,emacs-tmp-dir)))
+
+;;; Lockfiles unfortunately cause more pain than benefit
+(setq create-lockfiles nil)
+
+
+
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+(defun reload-config ()
+  (interactive)
+  (load-file (concat user-emacs-directory "init.el")))

e4439c3cd3c3be3e0f23d0368e9224730017d1f4
mark <mark@dixon.se>
2021-06-10 22:36:18 +0200

    Added /Users/mark/.emacs.d/init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+(load "~/.emacs.d/bootstrap.el" nil t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-download ob-js javascript company-org-block org-super-links nord-theme powershell epresent writeroom-mode which-key vterm use-package undo-tree toc-org sudo-edit solarized-theme smex shackle rainbow-delimiters python-mode projectile perspective org-bullets neotree markdown-mode magit ivy-rich ivy-posframe general exec-path-from-shell emojify doom-themes doom-modeline dashboard-project-status counsel avy async)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number-current-line ((t (:inherit default :background "#5E81AC"))))
+ '(org-block ((t (:background "#3B4252"))))
+ '(org-block-begin-line ((t (:foreground "#81A1C1" :background "#434C5E"))))
+ '(org-block-end-line ((t (:foreground "#81A1C1" :background "#434C5E")))))

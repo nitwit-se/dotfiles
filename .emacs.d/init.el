bb3b44a5ddbc4e3e182efc34700f2dc243b8df5a
mark <mark@dixon.se>
2021-06-29 19:56:54 +0200

    Added

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-download ob-js javascript company-org-block org-super-links nord-theme powershell epresent writeroom-mode which-key vterm use-package undo-tree toc-org sudo-edit solarized-theme smex shackle rainbow-delimiters python-mode projectile perspective org-bullets neotree markdown-mode magit ivy-rich ivy-posframe general exec-path-from-shell emojify doom-themes doom-modeline dashboard-project-status counsel avy async)))
+   '(ox-rst org-download ob-js javascript company-org-block org-super-links nord-theme powershell epresent writeroom-mode which-key vterm use-package undo-tree toc-org sudo-edit solarized-theme smex shackle rainbow-delimiters python-mode projectile perspective org-bullets neotree markdown-mode magit ivy-rich ivy-posframe general exec-path-from-shell emojify doom-themes doom-modeline dashboard-project-status counsel avy async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

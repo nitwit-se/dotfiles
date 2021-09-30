c6d8b9ed51c7adccf5b7fe04dd7b06b54f52334c
Mark Dixon <mark@dixon.se>
2021-09-30 12:36:11 +0200

    Updated files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,8 +6,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-show-quick-access t nil nil "Numbers are helpful.")
+ '(custom-safe-themes
+   '("2e9d290b3a2e3dae153566b105639c6ddcfa0300b7d97f0791e56a1229b1eccf" default))
  '(package-selected-packages
-   '(org-superstar consult guess-language ox-rst org-download ob-js javascript company-org-block org-super-links nord-theme powershell epresent writeroom-mode which-key vterm use-package undo-tree toc-org sudo-edit solarized-theme smex shackle rainbow-delimiters python-mode projectile perspective org-bullets neotree markdown-mode magit ivy-rich ivy-posframe general exec-path-from-shell emojify doom-themes doom-modeline dashboard-project-status counsel avy async)))
+   '(svg-lib frame quelpa-use-package quelpa straight bespoke-themes svg-tag-mode mu4e-column-faces org-beautify-theme chinolor-theme org-mu4e mu4e org-mime ox-hugo org-superstar consult guess-language ox-rst org-download ob-js javascript company-org-block org-super-links nord-theme powershell epresent writeroom-mode which-key vterm use-package undo-tree toc-org sudo-edit solarized-theme smex shackle rainbow-delimiters python-mode projectile perspective org-bullets neotree markdown-mode magit ivy-rich ivy-posframe general exec-path-from-shell emojify doom-themes doom-modeline dashboard-project-status counsel avy async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,4 +18,6 @@
  '(line-number-current-line ((t (:inherit default :background "#5E81AC"))))
  '(org-block ((t (:background "#3B4252"))))
  '(org-block-begin-line ((t (:foreground "#81A1C1" :background "#434C5E"))))
- '(org-block-end-line ((t (:foreground "#81A1C1" :background "#434C5E")))))
+ '(org-block-end-line ((t (:foreground "#81A1C1" :background "#434C5E"))))
+ '(org-ellipsis ((t (:foreground "forest green" :underline nil))))
+ '(org-tag ((t (:foreground "#696969" :weight normal :height 0.75)))))

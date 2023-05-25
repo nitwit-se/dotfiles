5f033bc843e76a87a7ebf7a900c048d6b7c9f7e1
Mark Dixon <mark@climateview.global>
2023-05-25 14:21:18 +0200

    Emacs 29 rework

--- .emacs.d/bootstrap.el
+++ .emacs.d/bootstrap.el
@@ -1,4 +1,3 @@
-
 ;; Bootstrapping code taken and modified from:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Sanemacs version 0.3.0 ;;;
@@ -14,52 +13,39 @@
                                ;; restore after startup
                                (setq gc-cons-threshold 800000)))
 
-;; https://blog.sumtypeofway.com/posts/emacs-config.html
-;; There appears to be some sort of kerfuffle regarding ELPA’s GPG keys. The details are beyond me,
-;; but installing this package silences associated warnings.
-;;(use-package gnu-elpa-keyring-update)
+
+(setq warning-minimum-level :emergency)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 
 
 ;;; Disable menu-bar, tool-bar, and scroll-bar.
-(if (fboundp 'menu-bar-mode)
-    (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode)
-    (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode)
-    (scroll-bar-mode -1))
+;;(if (fboundp 'menu-bar-mode)
+;;    (menu-bar-mode -1))
+;;(if (fboundp 'tool-bar-mode)
+;;    (tool-bar-mode -1))
+;;(if (fboundp 'scroll-bar-mode)
+;;    (scroll-bar-mode -1))
 
 (add-to-list 'initial-frame-alist '(fullscreen . fullheight))
 
-;;; Fix this bug:
-;;; https://www.reddit.com/r/emacs/comments/cueoug/the_failed_to_download_gnu_archive_is_a_pretty/
-(when (version< emacs-version "26.3")
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
-
-;;; Setup package.el
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'load-path "~/.emacs.d/packages/")
-(unless package--initialized (package-initialize))
+(when (memq window-system '(mac ns))
+  (add-to-list 'default-frame-alist '(ns-appearance . dark)) ; nil for dark text
+  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t)))
 
 
-
-;; Setup use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
-(setq use-package-always-ensure t)
-
-
-
-
-(use-package exec-path-from-shell
-   :ensure t)
-
-(when (memq window-system '(mac ns x))
-   (exec-path-from-shell-initialize))
 
 ;;; Put Emacs auto-save and backup files to /tmp/ or C:/Temp/
 (defconst emacs-tmp-dir (expand-file-name (format "emacs%d" (user-uid)) temporary-file-directory))
@@ -78,11 +64,20 @@
 
 (setq org-roam-v2-ack t)
 
+(straight-use-package 'org)
+
+(use-package org
+  :ensure org-contrib
+  ;; The rest of your org-mode configuration
+)
+
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
 
-(defun reload-config ()
-  (interactive)
-  (load-file (concat user-emacs-directory "init.el")))
+
+;;(defun reload-config ()
+;;  (interactive)
+;;  (load-file (concat user-emacs-directory "init.el")))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,23 +1,30 @@
 ;;; -*- lexical-binding: t -*-
 (load "~/.emacs.d/bootstrap.el" nil t)
+
+(set-language-environment "UTF-8")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-show-quick-access t nil nil "Numbers are helpful.")
  '(custom-safe-themes
-   '("2e9d290b3a2e3dae153566b105639c6ddcfa0300b7d97f0791e56a1229b1eccf" default))
- '(package-selected-packages
-   '(org-bars svg-lib frame quelpa-use-package quelpa straight bespoke-themes svg-tag-mode mu4e-column-faces org-beautify-theme chinolor-theme org-mu4e mu4e org-mime ox-hugo org-superstar consult guess-language ox-rst org-download ob-js javascript company-org-block org-super-links nord-theme powershell epresent writeroom-mode which-key vterm use-package undo-tree toc-org sudo-edit solarized-theme smex shackle rainbow-delimiters python-mode projectile perspective org-bullets neotree markdown-mode magit ivy-rich ivy-posframe general exec-path-from-shell emojify doom-themes doom-modeline dashboard-project-status counsel avy async)))
+   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "5dbdb4a71a0e834318ae868143bb4329be492dd04bdf8b398fb103ba1b8c681a" "6bf841f77d5eb01455d82ae436e3e25277daaef4ee855a3572589dad1b3ac4b3" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" "91c008faf603a28d026957120a5a924a3c8fff0e12331abf5e04c0e9dd310c65" "0ed3d96a506b89c1029a1ed904b11b5adcebeb2e0c16098c99c0ad95cb124729" "b6c43bb2aea78890cf6bd4a970e6e0277d2daf0075272817ea8bb53f9c6a7f0a" "e7820b899036ae7e966dcaaec29fd6b87aef253748b7de09e74fdc54407a7a02" "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" "e9d47d6d41e42a8313c81995a60b2af6588e9f01a1cf19ca42669a7ffd5c2fde" "d43860349c9f7a5b96a090ecf5f698ff23a8eb49cd1e5c8a83bb2068f24ea563" "0f220ea77c6355c411508e71225680ecb3e308b4858ef6c8326089d9ea94b86f" default))
+ '(safe-local-variable-values
+   '((org-hugo-section . "cvwiki")
+     (org-hugo-base-dir . "~/proj/hugo/climateos-wiki"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(line-number-current-line ((t (:inherit default :background "#5E81AC"))))
- '(org-block ((t (:background "#3B4252"))))
- '(org-block-begin-line ((t (:foreground "#81A1C1" :background "#434C5E"))))
- '(org-block-end-line ((t (:foreground "#81A1C1" :background "#434C5E"))))
- '(org-ellipsis ((t (:foreground "forest green" :underline nil))))
- '(org-tag ((t (:foreground "#696969" :weight normal :height 0.75)))))
+ '(italic ((t (:inherit nano-salient))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "dim gray" :bold nil :strike-through t))))
+ '(org-level-2 ((t (:inherit nano-default))))
+ '(org-level-3 ((t (:inherit nano-default))))
+ '(org-level-4 ((t (:inherit nano-default))))
+ '(org-level-5 ((t (:inherit nano-default))))
+ '(org-level-6 ((t (:inherit nano-default))))
+ '(org-level-7 ((t (:inherit nano-default))))
+ '(org-level-8 ((t (:inherit nano-default))))
+ '(org-table ((t (:inherit nano-default)))))
